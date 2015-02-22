5ee3d3511bc0ffbb108e320d009594844a23ed8d
Denis Efremov <yefremov.denis@gmail.com>
2015-02-22 18:55:27 +0300

    system instead of exec&&fork

--- lib/Linux/Kernel/Plugin/Unpack.pm
+++ lib/Linux/Kernel/Plugin/Unpack.pm
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 
 use Getopt::Long qw(:config gnu_compat no_permute no_getopt_compat pass_through);
+use File::Spec::Functions qw/catdir/;
 
 sub process_options
 {
@@ -35,16 +36,13 @@ sub action
 {
    my ($self, $opts) = @_;
 
-   my $pid = fork();
-   die "can't fork: $!"
-      unless defined $pid;
-
-   unless ($pid) {
-      print "UNPACKING $opts->{file} to directory $self->{dir}\n";
-      close STDIN;
-      close STDOUT;
-      close STDERR;
-      exec('tar', 'xf', $opts->{file}, '-C', $self->{dir});
+   print "UNPACKING $opts->{file} to directory $self->{dir}\n";
+   system('tar', 'xf', $opts->{file}, '-C', $self->{dir}) == 0 or
+      die "UNPACK FAIL\n";
+   my $dir = $opts->{file} =~ s/\.tar\.(?:\w){2,3}\z//r;
+   $dir = catdir $self->{dir}, $dir;
+   if (-d $dir) {
+      $opts->{'kernel-dir'} = $dir
    }
 }
 
