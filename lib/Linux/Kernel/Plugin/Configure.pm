86e10ad6362b043eb37a2f61178829b0cef9f083
Denis Efremov <yefremov.denis@gmail.com>
2015-02-25 00:18:46 +0300

    configure plugin

--- lib/Linux/Kernel/Plugin/Configure.pm
+++ lib/Linux/Kernel/Plugin/Configure.pm
@@ -4,9 +4,36 @@ use warnings;
 use strict;
 
 use Getopt::Long qw(:config gnu_compat no_permute no_getopt_compat pass_through);
-use File::Slurp qw/read_file/;
 use List::Util qw/any/;
+use IO::Handle;
+use IO::Select;
+use File::Spec::Functions qw/catfile/;
+require 'sys/ioctl.ph';
 
+
+sub _read_config
+{
+   my $file = $_[0];
+   my %config;
+
+   open my $f, '<', $file
+      or die "Can't open $file.\n";
+
+   while(<$f>) {
+      chomp;
+      s/#.*+//;
+      if (m/\A\h*+\Z/) {
+         next
+      }
+      if (m/\A\h*+(\w++)\h++(yes|mod|no)\h*+\Z/) {
+         $config{$1} = $2
+      } else {
+         warn "Error in line '$_'. Ignoring.\n"
+      }
+   }
+
+   \%config
+}
 
 sub process_options
 {
@@ -17,7 +44,6 @@ sub process_options
       'plugin-configure-file=s' => \$file,
    ) or die("Error in command line arguments\n");
 
-
    unless (any {$_ eq 'unpack'} @{$config->{plugins}}) {
       die "Unpack plugin should be loaded.\n"
    }
@@ -25,15 +51,19 @@ sub process_options
    die "Option --plugin-configure-file should be provided.\n"
       unless $file;
 
+   my ($cfg, $date);
    if (-f $file && -r _) {
-      $file = read_file $file
+      $date = (stat(_))[9];
+      $cfg = _read_config $file;
+      die "FAIL: Configure file is empty.\n"
+         unless %$cfg;
    } else {
-      die "Can't access file $file.\n"
+      die "FAIL: Can't access file $file.\n"
    }
 
    $config->{'configure-file'} = $file;
 
-   bless { file => $file }, $self
+   bless { file => $file, config => $cfg, date => $date }, $self
 }
 
 sub priority
@@ -43,7 +73,109 @@ sub priority
 
 sub action
 {
-   ...
+   my ($self, $opts) = @_;
+
+   return undef
+      unless exists $opts->{'kernel-dir'};
+
+   die "PLUGINS CONFLICT\n"
+      if exists $opts->{'kernel-config'};
+
+   my $date = (stat($self->{file}))[9];
+   if ($date > $self->{date}) {
+      print "CONFIGURE: Rereading configuration file $self->{file}.\n";
+      $self->{config} = _read_config $self->{file};
+      die "FAIL: CONFIGURE: Configure file is empty.\n"
+         unless %{$self->{config}};
+   }
+   my %config = %{$self->{config}}; # copy
+
+   my ($parent_read, $parent_write);
+   my ($child_read,  $child_write);
+
+   pipe($parent_read, $child_write) and
+   pipe($child_read,  $parent_write) or
+      die "FAIL: CONFIGURE: Failed to setup pipe: $!\n";
+   $parent_write->autoflush(1);
+   $child_write->autoflush(1);
+   #autoflush STDOUT 1;
+   $child_read->blocking(1);
+   $parent_read->blocking(1);
+
+
+   if (my $pid = fork()) {
+      close $parent_read; close $parent_write;
+
+      my %answers = (
+         yes => 'y',
+         no  => 'n',
+         mod => 'm',
+         y   => 'y',
+         n   => 'n',
+         m   => 'm',
+         module => 'm'
+      );
+
+      my $s = IO::Select->new();
+      $s->add($child_read);
+      my $fd;
+      my $lastline = '';
+
+      while (1) {
+         $fd = undef;
+         ($fd) = $s->can_read(1);
+         if ($fd) {
+            my $size = pack("L", 0);
+            $child_read->ioctl(FIONREAD(), $size);
+            $size = unpack("L", $size);
+
+            last unless $size;
+
+            $child_read->read(my $c, $size);
+            #print "$c";
+            $lastline .= $c;
+            if ($lastline =~ m/\[[^]]*+\]:?\h*+(\(NEW\)\h*+)?\Z/) {
+               my $ok = 0;
+               foreach my $k (keys %config) {
+                  if (rindex($lastline, $k) != -1) {
+                     print $child_write "$answers{$config{$k}}\n";
+                     print "CONFIGURE: SWITCH: $k $config{$k}\n";
+                     delete $config{$k};
+                     $ok = 1;
+                    last
+                  }
+               }
+
+               print $child_write "\n"
+                  unless $ok;
+               $lastline = '';
+            }
+         }
+      }
+
+      waitpid($pid, 0);
+
+      foreach (keys %config) {
+         print STDERR "CONFIGURE: UNDEF: $_\n"
+      }
+   } else {
+      close $child_read; close $child_write;
+
+      chdir $opts->{'kernel-dir'};
+
+      open STDOUT, '>&', $parent_write;
+      open STDIN,  '<&', $parent_read;
+      #system qw/make defconfig/; # TODO: do we really need this?
+      exec qw/make config/;
+   }
+
+   my $cfg_file = catfile $opts->{'kernel-dir'}, '.config';
+   if (-f $cfg_file) {
+      $opts->{'kernel-config'} = $cfg_file;
+      return $cfg_file
+   }
+
+   undef
 }
 
 
