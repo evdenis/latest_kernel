d979e18852aba5fc779de6e46defdb4c04133dc6
Denis Efremov <yefremov.denis@gmail.com>
2015-02-25 15:16:54 +0300

    prepare plugin

---
+++ lib/Linux/Kernel/Plugin/Prepare.pm
@@ -0,0 +1,46 @@
+package Linux::Kernel::Plugin::Prepare;
+
+use warnings;
+use strict;
+
+use Getopt::Long qw(:config gnu_compat no_permute no_getopt_compat pass_through);
+use POSIX ":sys_wait_h";
+
+sub process_options
+{
+   my ($self, $config) = @_;
+
+   bless {}, $self
+}
+
+sub priority
+{
+   35
+}
+
+sub action
+{
+   my ($self, $opts) = @_;
+
+   return undef
+      unless exists $opts->{'kernel-dir'};
+
+   my $pid = fork();
+   die "FAIL: can't fork $!"
+      unless defined $pid;
+
+   unless ($pid) {
+      print "PREPARE: $opts->{'kernel-dir'}\n";
+      chdir $opts->{'kernel-dir'};
+      open (STDIN,  '</dev/null');
+      open (STDOUT, '>/dev/null');
+      exec(qw/make modules_prepare/);
+   }
+
+   waitpid $pid, 0;
+
+   undef
+}
+
+
+1;
