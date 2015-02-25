0905ee5252e921cce89c37186488fac6c41c9867
Denis Efremov <yefremov.denis@gmail.com>
2015-02-25 23:21:23 +0300

    Exec plugin

--- lib/Linux/Kernel/Plugin/Exec.pm
+++ lib/Linux/Kernel/Plugin/Exec.pm
@@ -1,32 +1,70 @@
 package Linux::Kernel::Plugin::Exec;
 
+use warnings;
+use strict;
+
 use Getopt::Long qw(:config gnu_compat no_permute no_getopt_compat pass_through);
 
 sub process_options
 {
    my ($self, $config) = @_;
-   my $cmd;
+   my $file;
+   my $wait = 1;
 
    GetOptions(
-      'plugin-exec-command=s' => \$cmd,
+      'plugin-exec-file=s' => \$file,
+      'plugin-exec-wait!'  => \$wait,
    ) or die("Error in command line arguments\n");
 
-   die "Option --plugin-exec-command should be provided.\n"
-      unless $cmd;
+   die "Option --plugin-exec-file should be provided.\n"
+      unless $file;
 
-   $config->{'exec-command'} = $cmd;
+   unless (-f $file && -r _ && -x _) {
+      die "FAIL: Can't access file $file.\n"
+   }
 
-   bless { cmd => $cmd }, $self
+   $config->{'exec-file'} = $file;
+
+   bless { file => $file, wait => $wait }, $self
 }
 
 sub priority
 {
-   40
+   100
 }
 
 sub action
 {
-   ...
+   my ($self, $opts) = @_;
+
+   return undef
+      unless exists $opts->{'kernel-dir'};
+
+   my $pid = fork();
+   die "FAIL: can't fork $!"
+      unless defined $pid;
+
+   unless ($pid) {
+      print "EXEC: $self->{file}\n";
+      #chdir $opts->{'kernel-dir'};
+      close STDIN;
+      {
+         my %args;
+
+         foreach (keys $opts) {
+            unless (ref $opts->{$_}) {
+               $args{'--' . $_} = $opts->{$_}
+            }
+         }
+         exec($self->{file}, %args);
+      }
+   }
+
+   if ($self->{wait}) {
+      waitpid $pid, 0
+   }
+
+   undef
 }
 
 
